8f1df3135c45ce956047b023134a94ce2aafe50b
Takuya <r28.6sa@gmail.com>
2021-05-01 20:24:03 +0900

    email column should be gone

--- database/migrations/2021_04_29_144506_create_contacts_table.php
+++ database/migrations/2021_04_29_144506_create_contacts_table.php
@@ -16,7 +16,6 @@ class CreateContactsTable extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
             $table->timestamps();
         });
     }
